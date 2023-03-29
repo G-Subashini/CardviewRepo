99708d7f026c3822e7a62788b34bcd511dd7e0cd
G-Subashini <subashinigce@gmail.com>
2023-03-29 17:43:33 +0530

    commit1

---
+++ ControlTemplates/ControlTemplates/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ControlTemplates.View;
+
+namespace ControlTemplates
+{
+    public partial class App : Application
+    {
+        public App ()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new CardPage());
+        }
+
+        protected override void OnStart ()
+        {
+        }
+
+        protected override void OnSleep ()
+        {
+        }
+
+        protected override void OnResume ()
+        {
+        }
+    }
+}
+
---
+++ ControlTemplates/ControlTemplates/View/Controls/CardView.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ControlTemplates.View.Controls
+{
+    public class CardView : ContentView
+    {
+        public static readonly BindableProperty CardTitleProperty = BindableProperty.Create(nameof(CardTitle), typeof(string), typeof(CardView), string.Empty);
+        public static readonly BindableProperty CardDescriptionProperty = BindableProperty.Create(nameof(CardDescription), typeof(string), typeof(CardView), string.Empty);
+        public static readonly BindableProperty CardBorderColorProperty = BindableProperty.Create(nameof(BorderColor), typeof(Color), typeof(CardView), Color.RoyalBlue);
+        public static readonly BindableProperty CardColorProperty = BindableProperty.Create(nameof(CardColor), typeof(Color), typeof(CardView), Color.Yellow);
+        public static readonly BindableProperty IconBackgroundColorProperty = BindableProperty.Create(nameof(IconBackgroundColor), typeof(Color), typeof(CardView), Color.Gray);
+        public static readonly BindableProperty IconImageSourceProperty = BindableProperty.Create(nameof(IconImageSource), typeof(ImageSource), typeof(CardView), default(ImageSource));
+
+
+
+        public string CardTitle
+        {
+            get => (string)GetValue(CardTitleProperty);
+            set => SetValue(CardTitleProperty, value);
+        }
+
+        public string CardDescription
+        {
+            get => (string)GetValue(CardDescriptionProperty);
+            set => SetValue(CardDescriptionProperty, value);
+        }
+
+        public Color BorderColor
+        {
+            get => (Color)GetValue(CardBorderColorProperty);
+            set => SetValue(CardBorderColorProperty, value);
+        }
+
+        public Color CardColor
+        {
+            get => (Color)GetValue(CardColorProperty);
+            set => SetValue(CardColorProperty, value);
+        }
+        public Color IconBackgroundColor
+        {
+            get => (Color)GetValue(IconBackgroundColorProperty);
+            set => SetValue(IconBackgroundColorProperty, value);
+        }
+        public ImageSource IconImageSource
+        {
+            get => (ImageSource)GetValue(IconImageSourceProperty);
+            set => SetValue(IconImageSourceProperty, value);
+        }
+    }
+}
